41b49f7e8badf7cd857389f92b81d7595f954b22
Shovel Maman <sma@masterloop.com>
2019-06-12 09:43:34 +0200

    removed unused

--- packages/flutter_bloc/lib/masterloop_flutter_bloc.dart
+++
@@ -1,5 +0,0 @@
-library masterloop_flutter_bloc;
-
-export 'package:masterloop_flutter_bloc/src/provider.dart';
-export 'package:masterloop_flutter_bloc/src/bloc.dart';
-export 'package:masterloop_flutter_bloc/src/api.dart';
