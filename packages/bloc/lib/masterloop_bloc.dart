e1d3ddd7ed99033434198d7e49cc5421bcbe573c
Shovel Maman <sma@masterloop.com>
2019-06-17 15:41:13 +0200

    exporting new classes

--- packages/bloc/lib/masterloop_bloc.dart
+++ packages/bloc/lib/masterloop_bloc.dart
@@ -1,5 +1,7 @@
 library masterloop_bloc;
 
+export 'package:masterloop_bloc/src/base.dart';
+export 'package:masterloop_bloc/src/bloc_list.dart';
 export 'package:masterloop_bloc/src/devices.dart';
 export 'package:masterloop_bloc/src/device.dart';
 export 'package:masterloop_bloc/src/templates.dart';
